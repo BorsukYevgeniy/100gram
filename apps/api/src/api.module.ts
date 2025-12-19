d9bc7a09fdde56f583e77b692878d8f6a4939da0
BorsukYevgeniy <akimivdenis2@gmail.com>
2025-12-19 15:06:13 +0200

    Removed unnecessary files

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,11 +1,7 @@
 import { ConfigModule } from '@config';
 import { Module } from '@nestjs/common';
-import { ApiController } from './api.controller';
-import { ApiService } from './api.service';
 
 @Module({
   imports: [ConfigModule.forApi()],
-  controllers: [ApiController],
-  providers: [ApiService],
 })
 export class ApiModule {}
