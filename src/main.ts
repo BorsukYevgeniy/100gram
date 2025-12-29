17c9dba640b37128062342b367e698f92d800268
BorsukYevgeniy <akimivdenis2@gmail.com>
2025-12-29 12:44:30 +0200

    Deleted unnecesary comments in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
