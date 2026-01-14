bc561378ab7f8839140ee23b0d0e4ef9747d5755
BorsukYevgeniy <akimivdenis2@gmail.com>
2026-01-14 21:38:15 +0200

    Added file extension validation

--- src/common/interceptor/avatar.interceptor.ts
+++ src/common/interceptor/avatar.interceptor.ts
@@ -1,7 +1,18 @@
+import { BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+
+import { extname } from 'path';
 
 export const AvatarInterceptor = FileInterceptor('avatar', {
   limits: {
     fileSize: 7 * 1024 * 1024, // 7 MegeBytes
+  },
+
+  fileFilter(req, file, callback) {
+    if (['png', 'jpg', 'jpeg', 'svg'].includes(extname(file.originalname))) {
+      callback(null, true);
+    } else {
+      callback(new BadRequestException('Invalid file extension'), false);
+    }
   },
 });
